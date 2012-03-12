fc12d1fde3fd855f4b473fa7d9fee5b82ea20df9
Jukka Aittola <jaittola@iki.fi>
2012-03-12 12:25:06 +0200

    Changes by android sdk and tools update.

--- sql/run-sqlite.sh
+++ sql/run-sqlite.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec ~/Android/android-sdk-mac_x86/platform-tools/adb -s emulator-5554 shell "sqlite3 /data/data/com.ja.saillog/databases/SLDB.db $@"
+exec ~/Android/android-sdk-macosx/platform-tools/adb -s emulator-5554 shell "sqlite3 /data/data/com.ja.saillog/databases/SLDB.db $@"
