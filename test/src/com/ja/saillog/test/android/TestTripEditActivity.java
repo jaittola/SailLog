ec9ff9dc24cfcd654da8a757e67a8b438342c437
Jukka Aittola <jaittola@iki.fi>
2012-12-10 18:01:50 +0200

    test: removed unnecessary duplication.

--- test/src/com/ja/saillog/test/android/TestTripEditActivity.java
+++ test/src/com/ja/saillog/test/android/TestTripEditActivity.java
@@ -24,181 +24,179 @@ public class TestTripEditActivity extends ActivityUnitTestCase<TripEditActivity>
 
     protected void setUp() throws Exception {
         DBProvider.setProvider(new DBProvider() {
-                 protected TripDBInterface getTripDBInstance(Context context) {
-                     return tripdb;
-                 }
-                 protected TrackDBInterface getTrackDBInstance(Context context, String dbname) {
-                     return trackdb;
-                 }
-             });
+                protected TripDBInterface getTripDBInstance(Context context) {
+                    return tripdb;
+                }
+                protected TrackDBInterface getTrackDBInstance(Context context, String dbname) {
+                    return trackdb;
+                }
+            });
 
-         super.setUp();
-     }
+        super.setUp();
+    }
 
-     protected void tearDown() throws Exception {
-         super.tearDown();
-     }
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
 
-     public void testSetupFromEmpty() {
-         runTea(null);
+    public void testSetupFromEmpty() {
+        runTea(withoutTripId);
 
-         for (TextView v: allViews) {
-             Assert.assertEquals(v.getText().toString().length(), 0);
-         }
-     }
-     
-     public void testSetupWithId() {
-         tripdb.setupTrips();
-         runTea(tripdb.aTrip.tripId);
-         
-         Assert.assertEquals(tripdb.aTrip.tripName,
-                             tripNameText.getText().toString());
-         // TODO, should check the content of the rest of the fields.
-     }  
-     
-     public void testSelectExistingTrip() {
-         tripdb.setupTrips();
-         runTea(tripdb.aTrip.tripId);
-         
-         selectButton.performClick();
-         
-         Assert.assertEquals(tripdb.aTrip.tripId, tripdb.selectedTrip.tripId);
-         
-         Assert.assertTrue(isFinishCalled());
-     }
-     
-     
-     public void testSaveExistingTrip() {
-         tripdb.setupTrips();
-         runTea(tripdb.aTrip.tripId);
-         
-         saveButton.performClick();
-         
-         Assert.assertEquals(tripdb.aTrip.tripId, tripdb.updatedTrip.tripId);
+        for (TextView v: allViews) {
+            Assert.assertEquals(v.getText().toString().length(), 0);
+        }
+    }
 
-         Assert.assertFalse(isFinishCalled());
-     }
+    public void testSetupWithId() {
+        runTea(withTripId);
 
-     public void testSaveNewTrip() {
-         String tripName = "My new trip";
-         
-         tripdb.setupTrips();
-         runTea(null);
-         
-         tripNameText.setText(tripName);
-         saveButton.performClick();
-         
-         Assert.assertEquals(tripdb.insertedTrip.tripName, tripName);
+        Assert.assertEquals(tripdb.aTrip.tripName,
+                            tripNameText.getText().toString());
+        // TODO, should check the content of the rest of the fields.
+    }
 
-         Assert.assertFalse(isFinishCalled());
-     }
+    public void testSelectExistingTrip() {
+        runTea(withTripId);
 
-     public void testSaveNewTripWithEmptyDetails() {        
-         tripdb.setupTrips();
-         runTea(null);
+        selectButton.performClick();
 
-         saveButton.performClick();
-         
-         // TODO, how to verify the showing of the toast.
-         
-         Assert.assertNull(tripdb.insertedTrip);
-         Assert.assertNull(tripdb.updatedTrip);
+        Assert.assertEquals(tripdb.aTrip.tripId, tripdb.selectedTrip.tripId);
 
-         Assert.assertFalse(isFinishCalled());
-     }
-     
-     public void testSelectNewTrip() {
-         String tripName = "My trip for select";
-         
-         tripdb.setupTrips();
-         runTea(null);
-         
-         tripNameText.setText(tripName);
-         selectButton.performClick();
-         
-         Assert.assertEquals(tripName, tripdb.insertedTrip.tripName);
-         Assert.assertEquals(tripdb.insertedTrip.tripId, tripdb.selectedTrip.tripId);
+        Assert.assertTrue(isFinishCalled());
+    }
 
-         Assert.assertTrue(isFinishCalled());
-     }
-     
-     public void testDeleteTrip() {
-         tripdb.setupTrips();
-         runTea(tripdb.aTrip.tripId);
-         
-         tea.alreadyConfirmed = true;
-         deleteButton.performClick();
-         
-         Assert.assertEquals(tripdb.aTrip.tripId, tripdb.deletedTripId.longValue());
-         
-         Assert.assertTrue(isFinishCalled());
-     }
 
-     public void testDeleteWhenEmpty() {
-         tripdb.setupTrips();
-         runTea(null);
+    public void testSaveExistingTrip() {
+        tripdb.setupTrips();
+        runTea(withTripId);
 
-         deleteButton.performClick();
+        saveButton.performClick();
 
-         Assert.assertNull(tripdb.deletedTripId);
-         Assert.assertTrue(isFinishCalled());
-     }
-     
-     private void runTea(Long tripId) {
-         Intent intent = new Intent();
-         if (null != tripId) {
-             intent.putExtra("tripId", tripId.longValue());
-         }
-         tea = startActivity(intent, null, null);
-         tea.onStart();
-         
-         findViews();
-     }
+        Assert.assertEquals(tripdb.aTrip.tripId, tripdb.updatedTrip.tripId);
 
-     private void findViews() {
-         tripNameText = (EditText) tea.findViewById(R.id.tripNameText);
-         fromText = (EditText) tea.findViewById(R.id.fromText);
-         toText = (EditText) tea.findViewById(R.id.toText);
-         tripNameText = (EditText) tea.findViewById(R.id.legNameText);
-         startTime = (EditText) tea.findViewById(R.id.startTime);
-         endTime = (EditText) tea.findViewById(R.id.endTime);
-         totalDistanceText = (EditText) tea.findViewById(R.id.totalDistanceText);
-         totalEngineTimeText = (EditText) tea.findViewById(R.id.totalEngineTimeText);
-         totalSailingTimeText = (EditText) tea.findViewById(R.id.totalSailingTimeText);
-         selectButton = (Button) tea.findViewById(R.id.selectThisButton);
-         saveButton = (Button) tea.findViewById(R.id.saveTripButton);
-         deleteButton = (Button) tea.findViewById(R.id.deleteThisButton);
-  
+        Assert.assertFalse(isFinishCalled());
+    }
 
-         TextView[] myAllViews = {
-             tripNameText,
-             fromText,
-             toText,
-             startTime,
-             endTime,
-             totalDistanceText,
-             totalEngineTimeText,
-             totalSailingTimeText,
-         };
-         allViews = myAllViews;
-     }
+    public void testSaveNewTrip() {
+        String tripName = "My new trip";
 
-     private EditText tripNameText;
-     private EditText fromText;
-     private EditText toText;
-     private EditText startTime;
-     private EditText endTime;
-     private EditText totalDistanceText;
-     private EditText totalEngineTimeText;
-     private EditText totalSailingTimeText;
-     private Button selectButton;
-     private Button saveButton;
-     private Button deleteButton;
+        runTea(withoutTripId);
 
-     private TextView [] allViews;
+        tripNameText.setText(tripName);
+        saveButton.performClick();
 
-     private FakeTripDB tripdb = new FakeTripDB();
-     private FakeTrackDB trackdb = new FakeTrackDB();
+        Assert.assertEquals(tripdb.insertedTrip.tripName, tripName);
 
-     private TripEditActivity tea;
+        Assert.assertFalse(isFinishCalled());
+    }
+
+    public void testSaveNewTripWithEmptyDetails() {
+        runTea(withoutTripId);
+
+        saveButton.performClick();
+
+        // TODO, how to verify the showing of the toast.
+
+        Assert.assertNull(tripdb.insertedTrip);
+        Assert.assertNull(tripdb.updatedTrip);
+
+        Assert.assertFalse(isFinishCalled());
+    }
+
+    public void testSelectNewTrip() {
+        String tripName = "My trip for select";
+
+        runTea(withoutTripId);
+
+        tripNameText.setText(tripName);
+        selectButton.performClick();
+
+        Assert.assertEquals(tripName, tripdb.insertedTrip.tripName);
+        Assert.assertEquals(tripdb.insertedTrip.tripId, tripdb.selectedTrip.tripId);
+
+        Assert.assertTrue(isFinishCalled());
+    }
+
+    public void testDeleteTrip() {
+        runTea(withTripId);
+
+        tea.alreadyConfirmed = true;
+        deleteButton.performClick();
+
+        Assert.assertEquals(tripdb.aTrip.tripId, tripdb.deletedTripId.longValue());
+
+        Assert.assertTrue(isFinishCalled());
+    }
+
+    public void testDeleteWhenEmpty() {
+        runTea(withoutTripId);
+
+        deleteButton.performClick();
+
+        Assert.assertNull(tripdb.deletedTripId);
+        Assert.assertTrue(isFinishCalled());
+    }
+
+    private void runTea(boolean haveTripId) {
+        tripdb.setupTrips();
+
+        Intent intent = new Intent();
+        if (true == haveTripId) {
+            intent.putExtra("tripId", tripdb.aTrip.tripId);
+        }
+        tea = startActivity(intent, null, null);
+        tea.onStart();
+
+        findViews();
+    }
+
+    private void findViews() {
+        tripNameText = (EditText) tea.findViewById(R.id.tripNameText);
+        fromText = (EditText) tea.findViewById(R.id.fromText);
+        toText = (EditText) tea.findViewById(R.id.toText);
+        tripNameText = (EditText) tea.findViewById(R.id.legNameText);
+        startTime = (EditText) tea.findViewById(R.id.startTime);
+        endTime = (EditText) tea.findViewById(R.id.endTime);
+        totalDistanceText = (EditText) tea.findViewById(R.id.totalDistanceText);
+        totalEngineTimeText = (EditText) tea.findViewById(R.id.totalEngineTimeText);
+        totalSailingTimeText = (EditText) tea.findViewById(R.id.totalSailingTimeText);
+        selectButton = (Button) tea.findViewById(R.id.selectThisButton);
+        saveButton = (Button) tea.findViewById(R.id.saveTripButton);
+        deleteButton = (Button) tea.findViewById(R.id.deleteThisButton);
+
+
+        TextView[] myAllViews = {
+            tripNameText,
+            fromText,
+            toText,
+            startTime,
+            endTime,
+            totalDistanceText,
+            totalEngineTimeText,
+            totalSailingTimeText,
+        };
+        allViews = myAllViews;
+    }
+
+    private EditText tripNameText;
+    private EditText fromText;
+    private EditText toText;
+    private EditText startTime;
+    private EditText endTime;
+    private EditText totalDistanceText;
+    private EditText totalEngineTimeText;
+    private EditText totalSailingTimeText;
+    private Button selectButton;
+    private Button saveButton;
+    private Button deleteButton;
+
+    private TextView [] allViews;
+
+    private FakeTripDB tripdb = new FakeTripDB();
+    private FakeTrackDB trackdb = new FakeTrackDB();
+
+    private TripEditActivity tea;
+    
+    private static final boolean withTripId = true;
+    private static final boolean withoutTripId = false;
 }
