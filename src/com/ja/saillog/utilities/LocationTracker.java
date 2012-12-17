2368acb2279df5136c863fbd95535947fc44ed83
Jukka Aittola <jaittola@iki.fi>
2012-12-17 19:37:57 +0200

    LocationManager: Bugfix (multiple registration) and cleanup.
    Cleaned up location manager. Now it also does not register
    several times to the system location service, which happened
    before.

--- src/com/ja/saillog/utilities/LocationTracker.java
+++ src/com/ja/saillog/utilities/LocationTracker.java
@@ -13,63 +13,70 @@ import android.location.LocationProvider;
 import android.os.Bundle;
 
 public class LocationTracker implements LocationListener {
-	
-	public LocationTracker(Activity act) {
-		activity = act;
-	}
-	
-	public void setSinks(List<LocationSink> sinks) {
-		locationSinks = sinks;
-	}
-	
-	public void setEnabled(boolean isEnabled) {
 
-		if (null == locationManager) {
-			locationManager = (LocationManager) activity.getSystemService(Context.LOCATION_SERVICE);
-		}
+    public LocationTracker(Activity act) {
+        activity = act;
+    }
 
-		if (true == isEnabled) {
-			locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, locationMinimumInterval, 0, this);
-		}
-		else {
-			locationManager.removeUpdates(this);
-		}
-	}
-	
-	public void onLocationChanged(Location location) {
-	    if (locationSinks == null) {
-	        return;
-	    }
-	    
-		for (LocationSink sink: locationSinks) {
-			sink.updateLocation(location.getLatitude(),
-								location.getLongitude(),
-							    QuantityFactory.metersPerSecond(location.getSpeed()),
-							    location.getBearing(),
-							    location.getAccuracy(),
-							    location.getTime());
-		}
-	}
-	
-	public void onStatusChanged(String provider, int status, Bundle extras) {
-		// TODO, This isn't correct
-		boolean locationAvailable = (LocationProvider.OUT_OF_SERVICE != status);	
-		
-		if (locationSinks == null) {
-		    return;
-		}
-		
-		for (LocationSink sink: locationSinks) {
-			sink.setLocationAvailable(locationAvailable);
-		}
-	}
-	
-	public void onProviderEnabled(String provider) { }
-	
-	public void onProviderDisabled(String provider) { }
-	
-	private LocationManager locationManager;
-	private Activity activity;
-	private List<LocationSink> locationSinks;
-	private static final int locationMinimumInterval = 2000; // ms
+    public void setSinks(List<LocationSink> sinks) {
+        locationSinks = sinks;
+    }
+
+    public void setEnabled(boolean isEnabled) {
+        if (null == locationManager) {
+            locationManager = (LocationManager)
+                activity.getSystemService(Context.LOCATION_SERVICE);
+        }
+
+        // Make sure we do not request updates several times.
+        if (true == isEnabled && false == isRegistered) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                                                   locationMinimumInterval,
+                                                   0,
+                                                   this);
+        }
+        else {
+            locationManager.removeUpdates(this);
+        }
+
+        isRegistered = isEnabled;
+    }
+
+    public void onLocationChanged(Location location) {
+        if (locationSinks == null) {
+            return;
+        }
+
+        for (LocationSink sink: locationSinks) {
+            sink.updateLocation(location.getLatitude(),
+                                location.getLongitude(),
+                                QuantityFactory.metersPerSecond(location.getSpeed()),
+                                location.getBearing(),
+                                location.getAccuracy(),
+                                location.getTime());
+        }
+    }
+
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO, This isn't correct
+        boolean locationAvailable = (LocationProvider.OUT_OF_SERVICE != status);
+
+        if (locationSinks == null) {
+            return;
+        }
+
+        for (LocationSink sink: locationSinks) {
+            sink.setLocationAvailable(locationAvailable);
+        }
+    }
+
+    public void onProviderEnabled(String provider) { }
+
+    public void onProviderDisabled(String provider) { }
+
+    private LocationManager locationManager;
+    private Activity activity;
+    private List<LocationSink> locationSinks;
+    private static final int locationMinimumInterval = 2000; // ms
+    private boolean isRegistered = false;
 }
